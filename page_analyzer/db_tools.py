765204c369d369e4a13f033f3bd85d8268624178
alienflakes <alienflakes38@gmail.com>
2024-02-04 01:45:55 +0300

    edit: connection init method

--- page_analyzer/db_tools.py
+++ page_analyzer/db_tools.py
@@ -7,11 +7,15 @@ import datetime
 
 load_dotenv()
 DATABASE_URL = os.getenv('DATABASE_URL')
-conn = psycopg2.connect(DATABASE_URL)
+
+
+def db_connect():
+    return psycopg2.connect(DATABASE_URL)
 
 
 def db_execute(query, fetch=True, fetchall=False):
-    with conn.cursor(cursor_factory=NamedTupleCursor) as cursor:
+    with db_connect() as conn:
+        cursor = conn.cursor(cursor_factory=NamedTupleCursor)
         cursor.execute(query)
         if fetchall:
             return cursor.fetchall()
